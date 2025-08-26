ff31eccdffae984424c0ed4935ab5fb2d93e07df
Naveenraja00 <74108430+Naveenraja00@users.noreply.github.com>
2025-08-26 10:52:11 +0530

    Create inversify.types.ts

---
+++ src/app/inversify.types.ts
@@ -0,0 +1,11 @@
+const TYPES = {
+  Logger: Symbol.for("Logger"),
+  Validator: Symbol.for("Validator"),
+  EventBus: Symbol.for("EventBus"),
+  SoapClient: Symbol.for("SoapClient"),
+  HealthRepository: Symbol.for("HealthRepository"),
+  HelloRepository: Symbol.for("HelloRepository"),
+  TmsSoapRepository: Symbol.for("TmsSoapRepository"),
+};
+
+export { TYPES };
